58c0ee1248eab2e3ac6985feadfeba12ce5ef005
Harshit Yadav <61971255+Harshit-998@users.noreply.github.com>
2022-10-28 11:50:15 +0530

    Add files via upload

---
+++ snakeladder/src/main/java/com/example/snakeladder/Tile.java
@@ -0,0 +1,16 @@
+package com.example.snakeladder;
+
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Tile extends Rectangle {
+
+    public Tile(int x, int y){
+        setWidth(x);
+        setHeight(y);
+        setFill(Color.RED);
+        setStroke(Color.BLACK);
+
+    }
+}
